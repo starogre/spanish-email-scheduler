d95f34fe0948093b4fecfba16ec17cfae76ee5b1
Jon <jonshoob@gmail.com>
2025-07-13 19:06:21 -0400

    add firebase config file under api folder

---
+++ api/firebase-config.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  });
+} 
